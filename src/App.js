e283f83d9fef47ad8af6b6b4f38366131132ad47
Prathamesh Bhujbal <bhujbalprathamesh8@gmail.com>
2024-07-28 17:59:40 +0530

    First Push

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import GuestLoginSignup from './Components/GuestLoginSignup/GuestLoginSignup';
+import HotelLoginSignup from './Components/HotelLoginSignup/HotelLoginSignup';
+import './App.css';
+import './index.css';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/hotel" element={<div className="hotel-background full-screen"><HotelLoginSignup /></div>} />
+        <Route path="/guest" element={<div className="guest-background full-screen"><GuestLoginSignup /></div>} />
+        <Route path="/" element={<div>Welcome to the Hotel Management System</div>} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/Components/GuestLoginSignup/GuestLoginSignup.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import './GuestLoginSignup.css';
+import user_icon from '../Assets/person.png';
+import email_icon from '../Assets/email.png';
+import password_icon from '../Assets/password.png';
+import phone_icon from '../Assets/phone.png';
+import hotel from '../Assets/hotel.png';
+
+const GuestLoginSignup = () => {
+  const [action, setAction] = useState("Login");
+
+  return (
+    <div className='container'>
+      <div className="header">
+        <div className="text">{action} </div>
+        <div className="underline"></div>
+      </div>
+      <div className="inputs">
+        {action === "Login" ? <div></div> : <div className="input">
+          <img className="icon2" src={user_icon} alt="" />
+          <input type="text" placeholder="Name"/>
+        </div>}
+        <div className="input">
+          <img className="icon2" src={email_icon} alt="" />
+          <input type="email" placeholder="Email Id"/>
+        </div>
+        <div className="input">
+          <img className="icon2" src={password_icon} alt="" />
+          <input type="password" placeholder="Password"/>
+        </div>
+        {action === "Login" ? <div></div> : <div className='input'>
+          <img className="icon" src={phone_icon} alt="" />
+          <input type="Phone Number" placeholder='Phone no' />
+        </div>}
+        {action === "Login" ? <div></div> : <div className="input">
+          <img className="icon3" src={hotel} alt="" />
+          <input type="Address" placeholder='Address' />
+        </div>}
+      </div>
+      {action === "Register" ? <div></div> : <div className="forgot-password">forgot password?<span>Click Here!</span></div>}
+      <div className="submit-container">
+        <div className={action === "Login" ? "submit gray" : "submit"} onClick={() => { setAction("Register") }}>Register</div>
+        <div className={action === "Register" ? "submit gray" : "submit"} onClick={() => { setAction("Login") }}>Login</div>
+      </div>
+    </div>
+  );
+}
+
+export default GuestLoginSignup;
