3fce7cff5bfdf7627c3fb4405bcfc7065daa6feb
Prathamesh Bhujbal <bhujbalprathamesh8@gmail.com>
2024-07-29 22:32:30 +0530

    new

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import GuestLoginSignup from './Components/GuestLoginSignup/GuestLoginSignup';
 import HotelLoginSignup from './Components/HotelLoginSignup/HotelLoginSignup';
+import MainPage from './Components/MainPage/MainPage';
 import './App.css';
 import './index.css';
 
@@ -12,6 +13,7 @@ function App() {
         <Route path="/hotel" element={<div className="hotel-background full-screen"><HotelLoginSignup /></div>} />
         <Route path="/guest" element={<div className="guest-background full-screen"><GuestLoginSignup /></div>} />
         <Route path="/" element={<div>Welcome to the Hotel Management System</div>} />
+        <Route path="/main" element={<MainPage />} />
       </Routes>
     </Router>
   );
---
+++ src/Components/MainPage/MainPage.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './MainPage.css';
+
+export const MainPage = () => {
+  return (
+    <div className='main-page'>
+      <h1>Hotel Management System</h1>
+      <div className="button-container">
+        <div className="button-section">
+          <button className="button">Insert a Room</button>
+        </div>
+        <div className="button-section">
+          <button className="button">Delete a Room</button>
+        </div>
+        <div className="button-section">
+          <button className="button">View Available Rooms</button>
+        </div>
+        <div className="button-section">
+          <button className="button">Update a Room</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MainPage;
